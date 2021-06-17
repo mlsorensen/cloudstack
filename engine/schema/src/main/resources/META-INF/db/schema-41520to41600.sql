468eefdf717798689f99c35573f5550b8f520ca4
Harikrishna Patnala <harikrishna.patnala@gmail.com>
2021-06-17 07:55:50 +0530

    Fix User vm response to show proper service offering and disk offerings

--- engine/schema/src/main/resources/META-INF/db/schema-41520to41600.sql
+++ engine/schema/src/main/resources/META-INF/db/schema-41520to41600.sql
@@ -100,7 +100,7 @@ VIEW `user_vm_view` AS
         `iso`.`name` AS `iso_name`,
         `iso`.`display_text` AS `iso_display_text`,
         `service_offering`.`id` AS `service_offering_id`,
-        `svc_disk_offering`.`uuid` AS `service_offering_uuid`,
+        `service_offering`.`uuid` AS `service_offering_uuid`,
         `disk_offering`.`uuid` AS `disk_offering_uuid`,
         `disk_offering`.`id` AS `disk_offering_id`,
         (CASE
@@ -117,7 +117,7 @@ VIEW `user_vm_view` AS
         END) AS `ram_size`,
         `backup_offering`.`uuid` AS `backup_offering_uuid`,
         `backup_offering`.`id` AS `backup_offering_id`,
-        `svc_disk_offering`.`name` AS `service_offering_name`,
+        `service_offering`.`name` AS `service_offering_name`,
         `disk_offering`.`name` AS `disk_offering_name`,
         `backup_offering`.`name` AS `backup_offering_name`,
         `storage_pool`.`id` AS `pool_id`,
@@ -191,11 +191,11 @@ VIEW `user_vm_view` AS
         LEFT JOIN `host` ON ((`vm_instance`.`host_id` = `host`.`id`)))
         LEFT JOIN `vm_template` ON ((`vm_instance`.`vm_template_id` = `vm_template`.`id`)))
         LEFT JOIN `vm_template` `iso` ON ((`iso`.`id` = `user_vm`.`iso_id`)))
-        LEFT JOIN `service_offering` ON ((`vm_instance`.`service_offering_id` = `service_offering`.`id`)))
-        LEFT JOIN `disk_offering` `svc_disk_offering` ON ((`vm_instance`.`service_offering_id` = `svc_disk_offering`.`id`)))
-        LEFT JOIN `disk_offering` ON ((`service_offering`.`disk_offering_id` = `disk_offering`.`id`)))
-        LEFT JOIN `backup_offering` ON ((`vm_instance`.`backup_offering_id` = `backup_offering`.`id`)))
         LEFT JOIN `volumes` ON ((`vm_instance`.`id` = `volumes`.`instance_id`)))
+        LEFT JOIN `service_offering` ON ((`vm_instance`.`service_offering_id` = `service_offering`.`id`)))
+        LEFT JOIN `disk_offering` `svc_disk_offering` ON ((`volumes`.`disk_offering_id` = `svc_disk_offering`.`id`)))
+        LEFT JOIN `disk_offering` ON ((`volumes`.`disk_offering_id` = `disk_offering`.`id`)))
+        LEFT JOIN `backup_offering` ON ((`vm_instance`.`backup_offering_id` = `backup_offering`.`id`)))
         LEFT JOIN `storage_pool` ON ((`volumes`.`pool_id` = `storage_pool`.`id`)))
         LEFT JOIN `security_group_vm_map` ON ((`vm_instance`.`id` = `security_group_vm_map`.`instance_id`)))
         LEFT JOIN `security_group` ON ((`security_group_vm_map`.`security_group_id` = `security_group`.`id`)))
@@ -285,6 +285,9 @@ CREATE VIEW `cloud`.`disk_offering_view` AS
     GROUP BY
         `disk_offering`.`id`;
 
+-- Adding dynamic scalable flag for service offering table
+ALTER TABLE `cloud`.`service_offering` ADD COLUMN `dynamic_scaling_enabled` tinyint(1) unsigned NOT NULL DEFAULT 1  COMMENT 'true(1) if VM needs to be dynamically scalable of cpu or memory';
+
 DROP VIEW IF EXISTS `cloud`.`service_offering_view`;
 CREATE VIEW `cloud`.`service_offering_view` AS
     SELECT
@@ -298,96 +301,6 @@ CREATE VIEW `cloud`.`service_offering_view` AS
         `service_offering`.`removed` AS `removed`,
         `disk_offering`.`use_local_storage` AS `use_local_storage`,
         `service_offering`.`system_use` AS `system_use`,
-        `disk_offering`.`customized_iops` AS `customized_iops`,
-        `disk_offering`.`min_iops` AS `min_iops`,
-        `disk_offering`.`max_iops` AS `max_iops`,
-        `disk_offering`.`hv_ss_reserve` AS `hv_ss_reserve`,
-        `disk_offering`.`bytes_read_rate` AS `bytes_read_rate`,
-        `disk_offering`.`bytes_read_rate_max` AS `bytes_read_rate_max`,
-        `disk_offering`.`bytes_read_rate_max_length` AS `bytes_read_rate_max_length`,
-        `disk_offering`.`bytes_write_rate` AS `bytes_write_rate`,
-        `disk_offering`.`bytes_write_rate_max` AS `bytes_write_rate_max`,
-        `disk_offering`.`bytes_write_rate_max_length` AS `bytes_write_rate_max_length`,
-        `disk_offering`.`iops_read_rate` AS `iops_read_rate`,
-        `disk_offering`.`iops_read_rate_max` AS `iops_read_rate_max`,
-        `disk_offering`.`iops_read_rate_max_length` AS `iops_read_rate_max_length`,
-        `disk_offering`.`iops_write_rate` AS `iops_write_rate`,
-        `disk_offering`.`iops_write_rate_max` AS `iops_write_rate_max`,
-        `disk_offering`.`iops_write_rate_max_length` AS `iops_write_rate_max_length`,
-        `disk_offering`.`cache_mode` AS `cache_mode`,
-        `disk_offering`.`disk_size` AS `root_disk_size`,
-        `service_offering`.`cpu` AS `cpu`,
-        `service_offering`.`speed` AS `speed`,
-        `service_offering`.`ram_size` AS `ram_size`,
-        `service_offering`.`nw_rate` AS `nw_rate`,
-        `service_offering`.`mc_rate` AS `mc_rate`,
-        `service_offering`.`ha_enabled` AS `ha_enabled`,
-        `service_offering`.`limit_cpu_use` AS `limit_cpu_use`,
-        `service_offering`.`host_tag` AS `host_tag`,
-        `service_offering`.`default_use` AS `default_use`,
-        `service_offering`.`vm_type` AS `vm_type`,
-        `service_offering`.`sort_key` AS `sort_key`,
-        `service_offering`.`is_volatile` AS `is_volatile`,
-        `service_offering`.`deployment_planner` AS `deployment_planner`,
-        `vsphere_storage_policy`.`value` AS `vsphere_storage_policy`,
-        GROUP_CONCAT(DISTINCT(domain.id)) AS domain_id,
-        GROUP_CONCAT(DISTINCT(domain.uuid)) AS domain_uuid,
-        GROUP_CONCAT(DISTINCT(domain.name)) AS domain_name,
-        GROUP_CONCAT(DISTINCT(domain.path)) AS domain_path,
-        GROUP_CONCAT(DISTINCT(zone.id)) AS zone_id,
-        GROUP_CONCAT(DISTINCT(zone.uuid)) AS zone_uuid,
-        GROUP_CONCAT(DISTINCT(zone.name)) AS zone_name,
-        IFNULL(`min_compute_details`.`value`, `cpu`) AS min_cpu,
-        IFNULL(`max_compute_details`.`value`, `cpu`) AS max_cpu,
-        IFNULL(`min_memory_details`.`value`, `ram_size`) AS min_memory,
-        IFNULL(`max_memory_details`.`value`, `ram_size`) AS max_memory
-    FROM
-        `cloud`.`service_offering`
-            INNER JOIN
-        `cloud`.`disk_offering_view` AS `disk_offering` ON service_offering.disk_offering_id = disk_offering.id
-            LEFT JOIN
-        `cloud`.`service_offering_details` AS `domain_details` ON `domain_details`.`service_offering_id` = `disk_offering`.`id` AND `domain_details`.`name`='domainid'
-            LEFT JOIN
-        `cloud`.`domain` AS `domain` ON FIND_IN_SET(`domain`.`id`, `domain_details`.`value`)
-            LEFT JOIN
-        `cloud`.`service_offering_details` AS `zone_details` ON `zone_details`.`service_offering_id` = `disk_offering`.`id` AND `zone_details`.`name`='zoneid'
-            LEFT JOIN
-        `cloud`.`data_center` AS `zone` ON FIND_IN_SET(`zone`.`id`, `zone_details`.`value`)
-			LEFT JOIN
-		`cloud`.`service_offering_details` AS `min_compute_details` ON `min_compute_details`.`service_offering_id` = `disk_offering`.`id`
-				AND `min_compute_details`.`name` = 'mincpunumber'
-			LEFT JOIN
-		`cloud`.`service_offering_details` AS `max_compute_details` ON `max_compute_details`.`service_offering_id` = `disk_offering`.`id`
-				AND `max_compute_details`.`name` = 'maxcpunumber'
-			LEFT JOIN
-		`cloud`.`service_offering_details` AS `min_memory_details` ON `min_memory_details`.`service_offering_id` = `disk_offering`.`id`
-				AND `min_memory_details`.`name` = 'minmemory'
-			LEFT JOIN
-		`cloud`.`service_offering_details` AS `max_memory_details` ON `max_memory_details`.`service_offering_id` = `disk_offering`.`id`
-				AND `max_memory_details`.`name` = 'maxmemory'
-			LEFT JOIN
-		`cloud`.`service_offering_details` AS `vsphere_storage_policy` ON `vsphere_storage_policy`.`service_offering_id` = `disk_offering`.`id`
-				AND `vsphere_storage_policy`.`name` = 'storagepolicy'
-    WHERE
-        `disk_offering`.`state`='Active'
-    GROUP BY
-        `service_offering`.`id`;
-
--- Adding dynamic scalable flag for service offering table
-ALTER TABLE `cloud`.`service_offering` ADD COLUMN `dynamic_scaling_enabled` tinyint(1) unsigned NOT NULL DEFAULT 1  COMMENT 'true(1) if VM needs to be dynamically scalable of cpu or memory';
-DROP VIEW IF EXISTS `cloud`.`service_offering_view`;
-CREATE VIEW `cloud`.`service_offering_view` AS
-    SELECT
-        `service_offering`.`id` AS `id`,
-        `disk_offering`.`uuid` AS `uuid`,
-        `disk_offering`.`name` AS `name`,
-        `disk_offering`.`display_text` AS `display_text`,
-        `disk_offering`.`provisioning_type` AS `provisioning_type`,
-        `disk_offering`.`created` AS `created`,
-        `disk_offering`.`tags` AS `tags`,
-        `disk_offering`.`removed` AS `removed`,
-        `disk_offering`.`use_local_storage` AS `use_local_storage`,
-        `disk_offering`.`system_use` AS `system_use`,
         `disk_offering`.`customized_iops` AS `customized_iops`,
         `disk_offering`.`min_iops` AS `min_iops`,
         `disk_offering`.`max_iops` AS `max_iops`,
@@ -435,7 +348,7 @@ CREATE VIEW `cloud`.`service_offering_view` AS
     FROM
         `cloud`.`service_offering`
             INNER JOIN
-        `cloud`.`disk_offering_view` AS `disk_offering` ON service_offering.id = disk_offering.id
+        `cloud`.`disk_offering_view` AS `disk_offering` ON service_offering.disk_offering_id = disk_offering.id
             LEFT JOIN
         `cloud`.`service_offering_details` AS `domain_details` ON `domain_details`.`service_offering_id` = `disk_offering`.`id` AND `domain_details`.`name`='domainid'
             LEFT JOIN
@@ -463,6 +376,7 @@ CREATE VIEW `cloud`.`service_offering_view` AS
         `disk_offering`.`state`='Active'
     GROUP BY
         `service_offering`.`id`;
+
 --;
 -- Stored procedure to do idempotent column add;
 -- This is copied from schema-41000to41100.sql
